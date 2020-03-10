53553c8988b676abb92997eb0778035416931b3a
Armaan Merchant <armaanmerchant17@gmail.com>
2020-03-10 10:52:52 -0400

    Intro to App Development with Swift Code Files

---
+++ 6 Functions/Functions.playground/Pages/A Single Piece of Work.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*:
+ ## A Single Piece of Work
+ 
+ You can wrap up code that you may want to use more than once in a function. Similar to the way you *declared* the names of constants using `let`, you can declare a function using `func`.
+ 
+ Because functions can contain multiple lines of code, the declaration is a little more complicated. Here is a very simple function:
+*/
+func rowTheBoat() {
+    print("Row, row, row your boat")
+    print("Gently down the stream")
+}
+rowTheBoat()
+/*:
+ When you've called a function in a playground, you'll see results appear in the sidebar. Calling the `rowTheBoat` function will print text in the console area below, too.
+
+ - callout(Exercise): Write your own function that prints something to the console, then call your new function. Pay attention to the example above so that you know where all of the parentheses `()` and braces `{}` should go.
+ What happens if you call your function more than once?
+
+*/
+
+
+
+
+
+
+
+//: Next, learn about how to think about tasks and divide them up in a way that makes sense.
+//:
+//: [Previous](@previous)  |  page 4 of 12  |  [Next: Breaking It Down](@next)
